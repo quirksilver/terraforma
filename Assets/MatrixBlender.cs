e919ce7d4daa8f9c2d6e7ff9faa8a6d9d37f8915
Delia Hamwood <delia.hamwood@gmail.com>
2014-11-27 23:18:11 +0000

    globe! with clever transitions from orthographic to perspective and back again. Changes in position + rotation still need some work

---
+++ Assets/MatrixBlender.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(Camera))]
+public class MatrixBlender : MonoBehaviour
+{
+	public static Matrix4x4 MatrixLerp(Matrix4x4 from, Matrix4x4 to, float time, float duration, bool easeIn)
+	{
+		Matrix4x4 ret = new Matrix4x4();
+		for (int i = 0; i < 16; i++){
+			//ret[i] = Mathf.Lerp(from[i], to[i], time);
+
+			if (easeIn)
+			{
+				ret[i] = easeInCubic(time, from[i], to[i]-from[i], duration);
+			}
+			else
+			{
+				ret[i] = easeOutCubic(time, from[i], to[i]-from[i], duration);
+			}
+		}
+		return ret;
+	}
+	
+	private IEnumerator LerpFromTo(Matrix4x4 src, Matrix4x4 dest, float duration, bool easeIn, Vector3 startPos, Vector3 endPos, Quaternion startRot, Quaternion endRot)
+	{
+		float startTime = Time.time;
+		while (Time.time - startTime < duration)
+		{
+			camera.projectionMatrix = MatrixLerp(src, dest, (Time.time - startTime) / duration, duration, easeIn);
+			camera.transform.position = Vector3.Slerp(startPos, endPos, (Time.time - startTime) / duration);
+			camera.transform.rotation = Quaternion.Slerp(startRot, endRot, (Time.time - startTime) / duration);
+
+			yield return 1;
+		}
+		camera.projectionMatrix = dest;
+		camera.transform.position = endPos;
+		camera.transform.rotation = endRot;
+	}
+
+	//cubic easing in - accelerating from zero velocity
+		
+		//time, start value, change in value, duration
+	public static float easeInCubic(float t, float b, float c, float d) {
+		t /= d;
+		return c*t*t*t + b;
+	}
+	
+	
+	
+	// cubic easing out - decelerating to zero velocity
+	
+	
+	public static float easeOutCubic(float t, float b, float c, float d) {
+		t /= d;
+		t--;
+		return c*(t*t*t + 1) + b;
+	}
+	
+	public Coroutine BlendToMatrix(Matrix4x4 targetMatrix, float duration, bool easeIn, Vector3 startPos, Vector3 endPos, Quaternion startRot, Quaternion endRot)
+	{
+		StopAllCoroutines();
+		return StartCoroutine(LerpFromTo(camera.projectionMatrix, targetMatrix, duration, easeIn, startPos, endPos, startRot, endRot));
+	}
+}
---
+++ Assets/PerspectiveSwitcher.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(MatrixBlender))]
+public class PerspectiveSwitcher : MonoBehaviour
+{
+	private Matrix4x4   ortho,
+	perspective;
+	public float        fov     = 60f,
+	near    = .3f,
+	far     = 1000f,
+	orthographicSize = 50f;
+	private float       aspect;
+	private MatrixBlender blender;
+	private bool        orthoOn;
+
+	public Vector3 orthoPos, perspectivePos;
+	public Vector3 orthoRotEuler, perspectiveRotEuler;
+
+	private Quaternion orthoRot, perspectiveRot;
+	
+	void Start()
+	{
+		aspect = (float) Screen.width / (float) Screen.height;
+		ortho = Matrix4x4.Ortho(-orthographicSize * aspect, orthographicSize * aspect, -orthographicSize, orthographicSize, near, far);
+		perspective = Matrix4x4.Perspective(fov, aspect, near, far);
+		camera.projectionMatrix = ortho;
+		orthoOn = true;
+		blender = (MatrixBlender) GetComponent(typeof(MatrixBlender));
+
+		orthoRot = Quaternion.Euler(orthoRotEuler);
+		perspectiveRot = Quaternion.Euler(perspectiveRotEuler);
+	}
+	
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			orthoOn = !orthoOn;
+			if (orthoOn)
+				blender.BlendToMatrix(ortho, 1f, false, perspectivePos, orthoPos, perspectiveRot, orthoRot);
+			else
+				blender.BlendToMatrix(perspective, 1f, true, orthoPos, perspectivePos, orthoRot, perspectiveRot);
+		}
+	}
+}
