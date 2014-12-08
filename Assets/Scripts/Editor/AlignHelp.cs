dd7c816031270e627a3cc200a1ec3f44f4eccf22
OvergroundLab <Zhugie.h@googlemail.com>
2014-12-08 12:30:19 +0000

    New globe and skybox

---
+++ Assets/Scripts/Editor/AlignHelp.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class MenuItems
+{
+    [MenuItem("Custom Commands/Align to sphere")]
+    private static void AlignToSphere()
+    {
+        RaycastHit hit = Object.FindObjectOfType<Globe>().RotAtPoint(Selection.activeGameObject.transform.position);
+        Selection.activeGameObject.transform.rotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
+        Selection.activeGameObject.transform.position = hit.point+hit.normal/10.0f;
+    }
+}
---
+++ Assets/Scripts/Map/Globe.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Globe : MonoBehaviour 
+{
+    public RaycastHit RotAtPoint(Vector3 pos)
+    {
+        RaycastHit hit;
+        Vector3 dir = transform.position - pos;
+        dir = dir.normalized;
+        Debug.DrawRay(pos, dir, Color.red, 120.0f);
+        int layerMask = 1 << 8;
+        Physics.Raycast(pos, dir, out hit, 1000,layerMask);
+        Debug.DrawRay(pos, hit.normal*10, Color.green, 120.0f);
+        return hit;
+    }
+}
